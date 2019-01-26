06df60337f4c4fc24ada36674854cd463719c1c5
Ece Sefercioğlu <ecenazsefer@hotmail.com>
2019-01-27 02:06:16 +0300

    Time Manager Life Manager

---
+++ GGJ - Home/Assets/Scripts/LifeManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LifeManager : MonoBehaviour
+{
+
+    public List<Sprite> LifeSprites;
+    public int lifeCount = 3; //4 tane life barı olacak
+
+
+    public void LoseLife()
+    {
+        gameObject.GetComponent<SpriteRenderer>().sprite = LifeSprites[--lifeCount];
+    }
+
+}
---
+++ GGJ - Home/Assets/Scripts/TimeManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimeManager : MonoBehaviour
+{
+     Text time;
+    GameController GM;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        time = this.GetComponent<Text>();
+        GM = GameObject.FindObjectOfType<GameController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateTime();
+        if (Time.timeSinceLevelLoad > 300)
+        {
+            GM.GameOver();
+        }
+    }
+    void UpdateTime()
+    {
+        float timer = Time.timeSinceLevelLoad;
+        string minutes = Mathf.Floor(timer / 60).ToString("00");
+        string seconds = (timer % 60).ToString("00");
+        time.text = minutes+":"+seconds;
+    }
+}
