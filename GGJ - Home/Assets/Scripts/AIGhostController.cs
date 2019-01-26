c1229742944ee6537ff38f7680a531bad0dee8a2
tuggame <46846562+tuggame@users.noreply.github.com>
2019-01-26 19:50:37 +0300

    Camera - Karakter Kontrolü - Ghost V1.0 - Animasyon Geçişleri
    Camera - Karakter Kontrolü - Ghost V1.0 - Animasyon Geçişleri

---
+++ GGJ - Home/Assets/Scripts/AIGhostController.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AIGhostController : MonoBehaviour
+{
+
+    private NavMeshAgent m_NavMeshAgent;
+    public GameObject m_Target;
+
+    public float startWaitTime;
+    private float waitTime;
+
+    private int randomSpot;
+    private Transform[] moveSpots;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_NavMeshAgent = GetComponent<NavMeshAgent>();
+        waitTime = startWaitTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        m_NavMeshAgent.SetDestination(m_Target.transform.position);
+        if (waitTime <= 0f)
+        {
+
+        }
+        else
+        {
+            waitTime -= Time.deltaTime;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Wall")
+        {
+            ChangeLayers(gameObject, "Not Solid");
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Wall")
+        {
+            ChangeLayers(gameObject, "Default");
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Wall")
+        {
+            // ChangeLayers(gameObject, "Not Solid");
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.tag == "Wall")
+        {
+            // ChangeLayers(gameObject, "Default");
+        }
+    }
+
+    private void ChangeLayers(GameObject gameObject, string newLayer)
+    {
+        gameObject.layer = LayerMask.NameToLayer(newLayer);
+        foreach (Transform child in gameObject.transform)
+        {
+            ChangeLayers(child.gameObject, newLayer);
+        }
+    }
+}
---
+++ GGJ - Home/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float moveSpeed;
+    public float runSpeed;
+
+    private Vector3 forward;
+    private Vector3 right;
+
+    private static Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // anim = transform.parent.GetComponent<Animator>();
+        anim = transform.GetComponent<Animator>();
+
+        forward = Camera.main.transform.forward;
+        forward.y = 0f;
+        forward = Vector3.Normalize(forward);
+
+        right = Quaternion.Euler(new Vector3(0f, 90f, 0f)) * forward;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetAxisRaw("HorizontalKey") != 0f || Input.GetAxisRaw("VerticalKey") != 0f)
+        {
+            Move();
+        }
+        else
+        {
+            anim.SetBool("IsWalking", false);
+            anim.SetBool("IsRunning", false);
+        }
+    }
+
+    private void Move()
+    {
+        bool isRun = Input.GetAxisRaw("RunKey") != 0f;
+        float speed = isRun ? moveSpeed : runSpeed;
+
+        if (isRun)
+        {
+            anim.SetBool("IsWalking", false);
+            anim.SetBool("IsRunning", true);
+        }
+        else
+        {
+            anim.SetBool("IsRunning", false);
+            anim.SetBool("IsWalking", true);
+        }
+
+        Vector3 direction = new Vector3(Input.GetAxisRaw("HorizontalKey"), 0f, Input.GetAxisRaw("VerticalKey"));
+        Vector3 rightMovement = right * speed * Time.deltaTime * Input.GetAxisRaw("HorizontalKey");
+        Vector3 upMovement = forward * speed * Time.deltaTime * Input.GetAxisRaw("VerticalKey");
+
+        Vector3 heading = Vector3.Normalize(rightMovement + upMovement);
+        transform.forward = heading; // Rotation
+        transform.position += rightMovement + upMovement; // Movement
+        // transform.parent.forward = heading; // Rotation
+        // transform.parent.position += rightMovement + upMovement; // Movement
+        
+        // transform.Rotate(heading);
+        // transform.Translate(transform.position + rightMovement + upMovement);
+    }
+}
