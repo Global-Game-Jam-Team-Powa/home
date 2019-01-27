b2b864e2cb13dfe29f91aa12346998a41f21838e
Ece Sefercioğlu <ecenazsefer@hotmail.com>
2019-01-27 07:16:38 +0300

    hello
    hello

---
+++ GGJ - Home/Assets/Scripts/Memory.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Memory : MonoBehaviour
+{
+
+    public Transform StartTransform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.StartTransform = this.transform;   
+    }
+
+    void GhostFeltTheMemory()
+    {
+        this.transform.SetPositionAndRotation(StartTransform.position,StartTransform.rotation);
+    }
+
+}
