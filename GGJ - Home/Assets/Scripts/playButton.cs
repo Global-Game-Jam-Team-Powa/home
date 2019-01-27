b87775293007c5ae66f416a05d1196c5c1911d8a
Baransel <bgulkanat@users.noreply.github.com>
2019-01-27 06:24:01 +0300

    updatee

---
+++ GGJ - Home/Assets/Scripts/playButton.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class playButton : MonoBehaviour
+{
+    public void play_buttonn_Onclick()
+    {
+        SceneManager.LoadScene("Game");
+    }
+}
